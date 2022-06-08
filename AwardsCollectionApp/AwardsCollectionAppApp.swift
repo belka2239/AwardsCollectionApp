608dfb2599c148a2698511e57649b57f18454cc9
belka2239 <belka2239@gmail.com>
2022-06-08 14:55:15 +0300

    Initial Commit

---
+++ AwardsCollectionApp/AwardsCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionAppApp.swift
+//  AwardsCollectionApp
+//
+//  Created by VG on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
